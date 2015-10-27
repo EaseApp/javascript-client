13e22c340947c55d2c23bf3a5060b8a038b1e596
CBXZero <cbesalke@gmail.com>
2015-10-26 21:35:48 -0500

    Added functional signIn and getApplicaitons functions to Ease Javascript API

--- ease.js
+++ ease.js
@@ -1,8 +1,46 @@
 function Ease(apiToken) {
 
   // Properties of API object
-  this.apiToken = apiToken;
+  this.apiToken = "";
 
   // Methods of API object
+
+  this.signIn = function(username, password) {
+    var data = {};
+    var currentEase = this;
+    data.username = username;
+    data.password = password;
+    $.ajax({
+      url: 'http://localhost:3001/users/sign_in',
+      type: "POST",
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(data),
+      async: false,
+      beforeSend: function(xhr) {
+        xhr.withCredentials = true;
+      }
+    }).complete(function(data) {
+      var response = JSON.parse(data.responseText);
+      currentEase.apiToken = response.api_token;
+    });
+  }
+
+  this.getApplications = function() {
+    console.log(this.apiToken);
+    $.ajax({
+      url: 'http://localhost:3001/users/applications',
+      type: "GET",
+      async: false,
+      contentType: 'application/json; charset=utf-8;',
+      headers: {"Authorization" : this.apiToken},
+      beforeSend: function(xhr) {
+        xhr.withCredentials = true;
+      }
+    }).complete(function(data) {
+      var response = JSON.parse(data.responseText);
+    });
+  }
+
+
 
 }
