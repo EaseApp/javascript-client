59ad2fb5b960a25142e039f8ec7dbf3710088358
CBXZero <cbesalke@gmail.com>
2015-11-10 22:28:34 -0600

    Started CRUD operations and established connection to Sync Server.
    CRUD operations are established, need to finish Read and then finish testing.
    Sync Server can be connected to, haven't tested actually syncing data though.

--- ease.js
+++ ease.js
@@ -3,28 +3,38 @@ function Ease(apiToken) {
   // Properties of API object
   this.apiToken = "";
   this.conn = undefined;
+  this.binds = [];
+  this.username = '';
+  this.appName = "";
+  this.appToken = "";
 
   // Methods of API object
 
   this.connect = function(application) {
     var currentEase = this;
-    //currentEase.conn = new WebSocket("ws://localhost:3001/sub");
-    currentEase.conn = new WebSocket("ws://localhost:3001/sub");
+    //currentEase.conn = new WebSocket("ws://localhost:3000/sub");
+    currentEase.conn = new WebSocket("ws://localhost:8000/sub");
     currentEase.conn.onclose = function(e) {
       console.log("Connection closed");
     };
     currentEase.conn.onmessage = function(e) {
       //To be refactored
     };
-  }
+  };
+  
+  this.close = function() {
+    var currentEase = this;
+    currentEase.conn.close();
+  };
 
   this.signIn = function(username, password) {
     var data = {};
     var currentEase = this;
     data.username = username;
     data.password = password;
+    currentEase.username = username;
     $.ajax({
-      url: 'http://localhost:3001/users/sign_in',
+      url: 'http://localhost:3000/users/sign_in',
       type: "POST",
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify(data),
@@ -36,7 +46,7 @@ function Ease(apiToken) {
       var response = JSON.parse(data.responseText);
       currentEase.apiToken = response.api_token;
     });
-  }
+  };
 
   this.signUp = function(username, password) {
     var data = {};
@@ -44,7 +54,7 @@ function Ease(apiToken) {
     data.username = username;
     data.password = password;
     $.ajax({
-      url: 'http://localhost:3001/users/sign_up',
+      url: 'http://localhost:3000/users/sign_up',
       type: "POST",
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify(data),
@@ -56,11 +66,12 @@ function Ease(apiToken) {
       var response = JSON.parse(data.responseText);
       currentEase.apiToken = response.api_token;
     });
-  }
+  };
 
   this.getApplications = function() {
+    var response = "";
     $.ajax({
-      url: 'http://localhost:3001/users/applications',
+      url: 'http://localhost:3000/users/applications',
       type: "GET",
       async: false,
       contentType: 'application/json; charset=utf-8;',
@@ -69,13 +80,20 @@ function Ease(apiToken) {
         xhr.withCredentials = true;
       }
     }).complete(function(data) {
-      var response = JSON.parse(data.responseText);
+      response = JSON.parse(data.responseText);
+      console.log(response);
     });
-  }
+    return response;
+  };
+  
+  this.setApplication = function(token) {
+    this.appName = token.name;
+    this.appToken = token.app_token;
+  };
 
   this.deleteApplication = function(application) {
 	$.ajax({
-		url: 'http://localhost:3001/users/applications/' + application,
+		url: 'http://localhost:3000/users/applications/' + application,
 		type: "DELETE",
 		async: false,
 		contentType: 'application/json; charset=utf-8;',
@@ -86,11 +104,11 @@ function Ease(apiToken) {
 	}).complete(function(data) {
 		var resposne = JSON.parse(data.responseText);
 	});
-  }
+  };
 
   this.createApplication = function(application) {
 	  $.ajax({
-		  url: 'http://localhost:3001/users/applications/' + application,
+		  url: 'http://localhost:3000/users/applications/' + application,
 		  type: "POST",
 		  async: false,
 		  contentType: 'application/json; charset=utf-8;',
@@ -101,7 +119,7 @@ function Ease(apiToken) {
 	  }).complete(function(data) {
 		  var response = JSON.parse(data.responseText);
 	  });
-  }
+  };
 
   this.publish = function(data) {
     if(this.conn == undefined) {
@@ -109,7 +127,7 @@ function Ease(apiToken) {
     }
 
     this.conn.send(data);
-  }
+  };
 
   this.subscribe = function(application) {
     if(this.conn == undefined) {
@@ -121,17 +139,87 @@ function Ease(apiToken) {
     } else {
       this.setCallback(this.conn.send, application);
     }
-  }
+  };
   
-  this.setCallback = function(calling, argument) {
+  this.setCallback = function(argument) {
     if(this.conn.readyState === 1) {
-      calling(argument);
+      this.conn.send(argument);
     } else {
       var that = this;
       setTimeout(function () {
-        that.setCallback(calling, argument);
+        that.setCallback(argument);
       }, 1000);
     }
-  }
+  };
+  
+  this.sync = function() {
+    for(var i=0; i< this.binds.length; i++) {
+      this.conn.send(binds[i].html);
+    }
+  };
+  
+  this.save = function(data) {
+    $.ajax({
+      url: 'http://localhost:3000/data/' + this.username + '/' + this.appName,
+      type: "POST",
+      async: false,
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8;',
+      headers: {"Authorization" : this.appToken},
+      beforeSend: function(xhr) {
+        xhr.withCredentials = true;
+      }
+    }).complete(function(data) {
+      var response = JSON.parse(data.responseText);
+    }).error(function(error, status, errorThrown) {
+      console.log(JSON.stringify(data));
+      console.error(error);
+      console.error(status);
+      console.error(errorThrown);
+    });
+  };
+  
+  this.read = function(data) {
+    $.ajax({
+      url: 'http://localhost:3000/data/' + this.username + '/' + this.appName,
+      type: "GET",
+      async: false,
+      contentType: 'application/json; charset=utf-8;',
+      headers: {"Authorization" : this.appToken},
+      data: JSON.stringify(data),
+      beforeSend: function(xhr) {
+        xhr.withCredentials = true;
+      }
+    }).complete(function(data) {
+      console.log(data);
+      return data;
+    }).error(function(error, status, errorThrown) {
+      console.log(JSON.stringify(data));
+      console.error(error);
+      console.error(status);
+      console.error(errorThrown);
+    });
+  };
+  
+  this.delete = function(data) {
+    $.ajax({
+      url: 'http://localhost:3000/data/' + this.username + '/' + this.appName,
+      type: "DELETE",
+      async: false,
+      contentType: 'application/json; charset=utf-8;',
+      data: JSON.stringify(data),
+      headers: {"Authorization" : this.appToken},
+      beforeSend: function(xhr) {
+        xhr.withCredentials = true;
+      }
+    }).complete(function(data) {
+      return data;
+    }).error(function(error, status, errorThrown) {
+      console.log(JSON.stringify(data));
+      console.error(error);
+      console.error(status);
+      console.error(errorThrown);
+    });
+  };
 
 }
